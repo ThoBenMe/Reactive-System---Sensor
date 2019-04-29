6a776539445f640b749c3c856afb5afab0186e44
Thomas Meza <thomas_meza@web.de>
2019-04-29 23:25:24 +0200

    added project Reactive

---
+++ 01reactive/src/test/java/de/thro/inf/reactive/EmployeeHandlingTest.java
@@ -0,0 +1,13 @@
+package de.thro.inf.reactive;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeHandlingTest {
+
+    @Test
+    public void testNotify() {
+
+    }
+}
